1a9ae6c5ff96a078f63b3d0fa390e407db8f57c2
Palak Bhatt <>
2023-03-20 12:02:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>WDD programs</title>
+        <script>
+            function gsfc()
+            {
+                var Name="Technology";
+                document.getElementById("SOT").innerHTML="gsfc INSTITUTE OF ENGINNERING AND"+ Name;
+
+            }
+
+        </script>
+    </head>
+    <body>
+        <p>####WDD Programs####</p>
+        <h1>gsfc University!!!</h1>
+        <p id="PIT"></p>
+        <button onclick="gsfc()">Click</button>
+    </body>
+</html>
